291c29aa92b18a438bcefff5a1bcee7698d1de8c
Reinout van Rees <reinout@vanrees.org>
2013-03-12 17:23:31 +0100

    Refactored main() into two separate functions. Added documentation.

--- nenslint/runner.py
+++ nenslint/runner.py
@@ -1,6 +1,7 @@
 """
-Main module of nenslint: it contains the actual function (:func:`main`) that's
-installed by ``setup.py`` as the ``nenslint`` command.
+:mod:`nenslint.runner` is the main module of nenslint: it contains the actual
+function (:func:`main`) that's installed by ``setup.py`` as the ``nenslint``
+command.
 
 The only thing that regularly needs to be changed in this file is the
 :attr:`CHECKER_MODULE_NAMES` variable with the list of actual checks.
@@ -39,17 +40,23 @@ def checker_classes():
     return classes
 
 
-def main():
-    """Function that's run as the ``nenslint`` console script.
+def run_actual_checkers():
+    """Run the actual checking process, going through all configured checkers.
+
+    For every checker, it calls the :meth:`nenslint.base.BaseChecker.looks_ok`
+    method implemented in that checker. If it returns ``False``, it prints the
+    checker's documentation (by calling
+    :meth:`nenslint.base.BaseChecker.documentation`) and it then prints the
+    commands suggested by
+    :meth:`nenslint.base.BaseChecker.suggested_commands`.
+
+    When no problems are detected, the next checker in line is called
+    directly. If there was a problem (and thus some suggested commands were
+    printed), we assume you want to go fix them right away. But we *do* ask
+    if you want to continue anyway, first. This helps with stepping over false
+    positives.
+
     """
-    parser = OptionParser()
-    parser.add_option("-v", "--verbose",
-                      action="store_true", dest="verbose", default=False,
-                      help="Verbose mode")
-    (options, args) = parser.parse_args()
-    loglevel = options.verbose and logging.DEBUG or logging.INFO
-    logging.basicConfig(level=loglevel,
-                        format="%(levelname)s: %(message)s")
     for checker_class in checker_classes():
         checker = checker_class()
         if checker.looks_ok():
@@ -63,3 +70,22 @@ def main():
         answer = raw_input("Continue checking? [y/N] ")
         if not 'y' in answer.lower():
             return
+
+
+def main():
+    """Function that's run as the ``nenslint`` console script.
+
+    Basically it only parses the commandline and checks whether it should
+    enable the logging in verbose or non-verbose mode. It then runs
+    :func:`run_actual_checkers` that does the real work.
+
+    """
+    parser = OptionParser()
+    parser.add_option("-v", "--verbose",
+                      action="store_true", dest="verbose", default=False,
+                      help="Verbose mode")
+    (options, args) = parser.parse_args()
+    loglevel = options.verbose and logging.DEBUG or logging.INFO
+    logging.basicConfig(level=loglevel,
+                        format="%(levelname)s: %(message)s")
+    run_actual_checkers()
